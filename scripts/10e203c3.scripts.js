0152718ff17afcc6bce5c7094696a44734eda1eb
Richard Littauer <richard.littauer@gmail.com>
2014-04-29 18:21:13 -0400

    Initial dist subtree commit

---
+++ scripts/10e203c3.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("talkaboutcodeApp",[]),angular.module("talkaboutcodeApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
